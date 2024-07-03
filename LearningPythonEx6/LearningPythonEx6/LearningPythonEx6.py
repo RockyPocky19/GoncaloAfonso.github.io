506bad30243cd232ad51f09cb75ad33e6a8f8b6f
Gonçalo Afonso <144245104+RockyPocky19@users.noreply.github.com>
2024-07-03 17:24:34 +0100

    Add files via upload

---
+++ LearningPythonEx6/LearningPythonEx6/LearningPythonEx6.py
@@ -0,0 +1,3 @@
+Slices = int(input("How many slices of pizza do you have?\n"))
+SlicesEaten = int(input("How many slices of pizza did you eat?\n"))
+print("You have more",Slices - SlicesEaten,"Slices")
